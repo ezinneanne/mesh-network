11db072c15a39185152e6f1e68c9d8017761caa3
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2025-07-10 14:02:39 +0100

    add changes

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../assets/offlinepaylogo_transparent.png')}
+        style={styles.logo}
+        accessibilityLabel="Offline Pay Logo"
+      />
       <Text style={styles.title}>Login</Text>
       <TextInput style={styles.input} placeholder="Email" />
       <TextInput style={styles.input} placeholder="Password" secureTextEntry />
-      <Button title="Log In" onPress={() => navigation.replace('Main')} />
+      <TouchableOpacity
+              style={styles.customButton}
+              onPress={() => navigation.navigate('Main')} 
+            >
+              <Text style={styles.customButtonText}>Log In</Text>
+      </TouchableOpacity>
+      
+      <Text style={styles.paragraph}>Don't have an account? Sign Up Instead </Text> 
     </View>
   );
 }
@@ -15,5 +27,31 @@ export default function LoginScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 20, justifyContent: 'center' },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
-  input: { borderWidth: 1, marginBottom: 15, padding: 10, borderRadius: 5 }
+  input: { borderWidth: 1, marginBottom: 15, padding: 10, borderRadius: 5 },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+    borderRadius: 75,
+  },
+  customButton: {
+    backgroundColor: '#6a78f0',
+    width: '100%',
+    minHeight: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    paddingVertical: 12,
+    marginTop: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 8,
+  },
+  customButtonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
